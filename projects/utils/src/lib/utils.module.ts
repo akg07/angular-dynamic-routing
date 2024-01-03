d3800f313445c0e7ae4f21de139a3c6662a8a932
Ayush <ayush3032@gmail.com>
2024-01-03 20:32:42 +0530

    create component || header, footer, loader

--- projects/utils/src/lib/utils.module.ts
+++ projects/utils/src/lib/utils.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
+import { LoaderComponent } from './loader/loader.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
+  
+    LoaderComponent,
+       HeaderComponent,
+       FooterComponent
   ],
   imports: [
   ],
