d726f3406de12258e1deb85641cd7f9850b7a467
Ayush <ayush3032@gmail.com>
2024-01-03 20:31:32 +0530

    create library || Utils

--- projects/dashboard/src/lib/dashboard.module.ts
+++ projects/dashboard/src/lib/dashboard.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { DashboardComponent } from './dashboard.component';
 import { ApplicationListComponent } from './application-list/application-list.component';
 import { ApplicationViewComponent } from './application-view/application-view.component';
 
@@ -7,14 +6,12 @@ import { ApplicationViewComponent } from './application-view/application-view.co
 
 @NgModule({
   declarations: [
-    DashboardComponent,
     ApplicationListComponent,
     ApplicationViewComponent
   ],
   imports: [
   ],
   exports: [
-    DashboardComponent
   ]
 })
 export class DashboardModule { }
