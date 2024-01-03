8498ce9d66e243127c483cc514e5707e00016206
Ayush <ayush3032@gmail.com>
2024-01-03 20:13:39 +0530

    create lib || Plans

---
+++ projects/plans/src/lib/plans.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-plans',
+  template: `
+    <p>
+      plans works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class PlansComponent {
+
+}
---
+++ projects/plans/src/lib/plans.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { PlansComponent } from './plans.component';
+
+
+
+@NgModule({
+  declarations: [
+    PlansComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    PlansComponent
+  ]
+})
+export class PlansModule { }
